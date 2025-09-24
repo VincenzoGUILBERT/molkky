5171a713b00e674f70294cadebf7bbf640fbf096
Teter24 <terence.mayombo@icloud.com>
2025-09-24 14:35:06 +0200

    WIP: travail en cours sur contact

---
+++ src/Repository/ContactRepository.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Repository;
+
+use App\Entity\Contact;
+use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\Persistence\ManagerRegistry;
+
+/**
+ * @extends ServiceEntityRepository<Contact>
+ */
+class ContactRepository extends ServiceEntityRepository
+{
+    public function __construct(ManagerRegistry $registry)
+    {
+        parent::__construct($registry, Contact::class);
+    }
+
+    /**
+     * Exemple : trouver les derniers contacts enregistrés
+     */
+    public function findLatest(int $limit = 10): array
+    {
+        return $this->createQueryBuilder('c')
+            ->orderBy('c.id', 'DESC')
+            ->setMaxResults($limit)
+            ->getQuery()
+            ->getResult();
+    }
+
+    /**
+     * Exemple : trouver par email
+     */
+    public function findByEmail(string $email): ?Contact
+    {
+        return $this->createQueryBuilder('c')
+            ->andWhere('c.email = :email')
+            ->setParameter('email', $email)
+            ->getQuery()
+            ->getOneOrNullResult();
+    }
+}
